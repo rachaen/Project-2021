b87f7589e527c0c6537696a9ab77a4bc3662a005
rachaen <rachaenlee@gmail.com>
2021-03-20 22:35:22 +0900

    Refactor code

--- browser101/shoppingList/main.js
+++ browser101/shoppingList/main.js
@@ -4,50 +4,48 @@ addEventListener('load', () => {
   const item_plus = document.querySelector('.item_plus');
 
   item_input.addEventListener('keydown', (event) => {
-    if (window.event.keyCode == 13) {
+    if (event.keyCode == 13) {
       addItem();
     }
   });
 
-  item_plus.addEventListener('click', (event) => {
+  item_plus.addEventListener('click', () => {
     addItem();
+    item_input.focus();
   });
 
-  function deleteLi(child) {
-    let grandparent = child.parentNode.parentNode;
-    grandparent.parentNode.removeChild(grandparent);
-  }
+  items.addEventListener('click', (event) => {
+    const id = event.target.dataset.id;
+    if (id) {
+      const toBeDeleted = document.querySelector(`.item[data-id="${id}"]`);
+      toBeDeleted.remove();
+    }
+  });
 
+  let id = 0;
   function addItem() {
     const value = item_input.value;
     const pluslist = document.createElement('li');
-    const plus_name = document.createElement('span');
-    const plus_delete = document.createElement('button');
-    const divider = document.createElement('div');
-    const delete_img = document.createElement('i');
-    const plus_content = document.createElement('div');
     if (!value || value === '' || value === ' ') {
       item_input.value = '';
       return false;
     }
-    divider.setAttribute('class', 'divider');
     pluslist.setAttribute('class', 'item');
-    plus_content.setAttribute('class', 'item_content');
-    plus_name.setAttribute('class', 'item_name');
-    plus_delete.setAttribute('class', 'item_delete');
-    delete_img.setAttribute('class', 'fas fa-trash-alt');
+    pluslist.setAttribute('data-id', id);
+    pluslist.innerHTML = `
+        <li class="item">
+          <div class="item_content">
+            <span class="item_name">${value}</span>
+            <button class="item_delete">
+              <i class="fas fa-trash-alt" data-id=${id}></i>
+            </button>
+          </div>
+        </li>
+    `;
+    id++;
 
-    plus_name.textContent = value;
-
-    plus_delete.appendChild(delete_img);
-    plus_content.appendChild(plus_name);
-    plus_content.appendChild(plus_delete);
-    pluslist.appendChild(plus_content);
     items.appendChild(pluslist);
 
-    plus_delete.addEventListener('click', (event) => {
-      deleteLi(event.currentTarget);
-    });
     items.scrollTop = items.scrollHeight;
 
     item_input.value = '';
